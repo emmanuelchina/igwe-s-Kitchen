9026c8d630e19d099c271e464999ca6bcf055900
Igwe Naza <igweemmauelchina.gmail.com>
2025-10-30 01:19:47 +0100

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,21 @@
+
+import Hero from './Hero';
+import About from './About';
+import Gallery from './Gallery';
+import Contact from './Contact';
+
+
+const Home=() =>{
+    return(
+        <div>
+        <Hero/>
+        <About/>
+        <Gallery/>
+        <Contact/>
+
+
+        </div>
+    )
+}
+
+export default Home
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from '../Components/Navbar';
 import Hero from '../Components/Hero';
 import About from '../Components/About';
-import Home from '../Home'
+import Home from '../Components/Home'
 import Gallery from '../Components/Gallery';
 import Contact from "../Components/Contact";
 import Footer from "../Components/Footer";
